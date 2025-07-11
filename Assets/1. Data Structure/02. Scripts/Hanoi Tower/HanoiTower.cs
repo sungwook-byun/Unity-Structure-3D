4328b63a2da684036ff4e3896eb8c1a3ca5112c3
sungwook-byun <qustjddnr11@naver.com>
2025-07-11 17:08:28 +0900

    [Study] Algorithm
    -- Linear Search
    -- Binary Search
    -- Depth First Search
    -- Breadth First Search

--- Assets/1. Data Structure/02. Scripts/Hanoi Tower/HanoiTower.cs
+++ Assets/1. Data Structure/02. Scripts/Hanoi Tower/HanoiTower.cs
@@ -2,21 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HanoiTower : MonoBehaviour
 {
     public enum HanoiLevel { Lv1 = 3, Lv2 , Lv3 };
-    public HanoiLevel hanoiLevel;
+    public HanoiLevel hanoiLevel = HanoiLevel.Lv1;
 
     public GameObject[] donutPrefab;
     public BoardBar[] bars; // L, C, R
 
     public TextMeshProUGUI countTextUI;
+    public Button answerButton;
 
     public static GameObject selectedDonut;
     public static bool isSelected;
     public static BoardBar currBar;
     public static int moveCount;
+
+    private void Awake()
+    {
+        answerButton.onClick.AddListener(HanoiAnswer);
+    }
 
     IEnumerator Start()
     {
@@ -43,5 +50,25 @@ public class HanoiTower : MonoBehaviour
             selectedDonut = null;
         }
         countTextUI.text = moveCount.ToString();
+    }
+
+    public void HanoiAnswer()
+    {
+        HanoiRoutine((int)hanoiLevel, 0, 1, 2);
+    }
+
+    private void HanoiRoutine(int n, int from, int temp, int to)
+    {
+        if(n == 1)
+            Debug.Log($"{n}번 도넛을 {from}에서 {to}로 이동");
+
+        else
+        {
+            HanoiRoutine(n - 1, from, to, temp);
+            Debug.Log($"{n}번 도넛을 {from}에서 {to}로 이동");
+
+            HanoiRoutine(n - 1, temp, from, to);
+        }
+
     }
 }
